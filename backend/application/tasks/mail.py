81c9eb7f9f684708198c16af3932d58cf86d1dc7
roctbb <roctbb@gmail.com>
2025-06-01 21:58:58 +0300

    DEbug

--- backend/application/tasks/mail.py
+++ backend/application/tasks/mail.py
@@ -2,15 +2,15 @@ from flask import current_app
 from application.infrastructure import mail, celery
 from flask_mail import Message
 
-
 @celery.task
 def send_flask_mail(recipient_email, subject, html):
-    msg = Message(
-        subject=subject,
-        recipients=[recipient_email],
-        html=html
-    )
+    with current_app.app_context():
+        msg = Message(
+            subject=subject,
+            recipients=[recipient_email],
+            html=html
+        )
 
-    print(mail.mail.server, mail.mail.port)
+        print(mail.mail.server, mail.mail.port)
 
-    mail.send(msg)
+        mail.send(msg)
